1131558e5e1d8414414e6e74cc0d5bb6a199f07a
Iván Uribe <ivanudg@hotmail.com>
2020-05-26 15:03:11 -0500

    RestServer - Usuario

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,20 @@
 // Puerto
 //==========================
 process.env.PORT = process.env.PORT || 3000;
+
+//===========================
+// Entorno
+//===========================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+//===========================
+// Base de Datos
+//===========================
+let urlBD;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlBD = 'mongodb://localhost:27017/cafe';
+} else {
+    urlBD = 'mongodb+srv://ivanudg:jAG9AmaTcPsJoGrQ@cafe-udemy-wy7ck.mongodb.net/cafe';
+}
+process.env.URLDB = urlBD;
--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 require('./config/config');
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 const bodyParser = require('body-parser');
 
@@ -9,36 +11,17 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-app.get('/usuario', (req, res) => {
-    res.json('Get Usuario');
-});
+app.use(require('./routes/usuario'));
 
-app.post('/usuario', (req, res) => {
+mongoose.connect(process.env.URLDB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+}, (err) => {
 
-    let body = req.body;
+    if (err) throw err;
+    console.log('Basde de datos ONLINE');
 
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        });
-    }
-
-    res.json({
-        persona: body
-    });
-});
-
-app.put('/usuario/:id', (req, res) => {
-
-    let id = req.params.id;
-    res.json({
-        id
-    });
-});
-
-app.delete('/usuario', (req, res) => {
-    res.json('Delete Usuario');
 });
 
 app.listen(process.env.PORT, () => {
