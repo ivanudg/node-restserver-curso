cb7a93c0a897fd9f79b0c63996e20f061019ae1a
Iván Uribe <ivanudg@hotmail.com>
2020-05-26 15:36:54 -0500

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -16,6 +16,6 @@ let urlBD;
 if (process.env.NODE_ENV === 'dev') {
     urlBD = 'mongodb://localhost:27017/cafe';
 } else {
-    urlBD = 'mongodb+srv://ivanudg:jAG9AmaTcPsJoGrQ@cafe-udemy-wy7ck.mongodb.net/cafe';
+    urlBD = process.env.MONGO_URI;
 }
 process.env.URLDB = urlBD;
